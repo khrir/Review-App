e9617a3d814bb05108c4dba59966dc43b5724729
Khrir <karllisson.15henrique@hotmail.com>
2022-09-05 20:58:15 -0300

    fix bug

--- lib/pages/cadastro/cadastrar.dart
+++ lib/pages/cadastro/cadastrar.dart
@@ -25,233 +25,245 @@ class _CadastrarState extends State<Cadastrar> {
           ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Stack(
-              children: [
-                const Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 7),
-                    child: Text(
-                      "Cadastrar",
-                      textScaleFactor: 2,
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => {
-                    Navigator.pop(context),
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back_ios,
-                    size: 25,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 35.0),
-                  child: Image.asset(
-                    "assets/review-logo.png",
-                    height: 125,
-                  ),
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 40, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Email",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                SizedBox(
-                  height: 35,
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      focusColor: Colors.white,
-                      hintText: "Digite seu email",
-                      hintStyle: TextStyle(color: Colors.black54),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8))),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 35, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Senha",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                SizedBox(
-                  height: 35,
-                  width: 300,
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      hintText: "Digite sua senha",
-                      hintStyle: TextStyle(color: Colors.black54),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8))),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 50,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.zero,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Stack(
+                        children: [
+                          const Center(
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 7),
+                              child: Text(
+                                "Cadastrar",
+                                textScaleFactor: 2,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () => {
+                              Navigator.pop(context),
+                            },
+                            icon: const Icon(
+                              Icons.arrow_back_ios,
+                              size: 25,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    onPressed: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  const LoginScreen()))
-                    },
-                    child: const Text(
-                      "Cadastrar",
-                      style: TextStyle(
-                        color: Colors.purple,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 35.0),
+                            child: Image.asset(
+                              "assets/review-logo.png",
+                              height: 125,
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: const TextSpan(children: <TextSpan>[
-                  TextSpan(
-                    text: "Ao criar uma conta você concorda com os ",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                    ),
-                  ),
-                  TextSpan(
-                    text: "termos e condições",
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                ]),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  children: <TextSpan>[
-                    const TextSpan(
-                      text: "Já possui cadastro? ",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40, bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              "Email",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: "Login!",
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const LoginScreen(),
-                              ));
-                        },
-                      style: const TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.white,
-                        fontSize: 19,
-                        fontWeight: FontWeight.bold,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          SizedBox(
+                            height: 35,
+                            width: 300,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                fillColor: Colors.white,
+                                filled: true,
+                                focusColor: Colors.white,
+                                hintText: "Digite seu email",
+                                hintStyle: TextStyle(color: Colors.black54),
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8))),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 35, bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              "Senha",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          SizedBox(
+                            height: 35,
+                            width: 300,
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                fillColor: Colors.white,
+                                filled: true,
+                                hintText: "Digite sua senha",
+                                hintStyle: TextStyle(color: Colors.black54),
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8))),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 30.0),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 50,
+                                ),
+                              ),
+                              onPressed: () => {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            const LoginScreen()))
+                              },
+                              child: const Text(
+                                "Cadastrar",
+                                style: TextStyle(
+                                  color: Colors.purple,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: const TextSpan(children: <TextSpan>[
+                            TextSpan(
+                              text: "Ao criar uma conta você concorda com os ",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "termos e condições",
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ]),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              const TextSpan(
+                                text: "Já possui cadastro? ",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              TextSpan(
+                                text: "Login!",
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              const LoginScreen(),
+                                        ));
+                                  },
+                                style: const TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.white,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 21),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              onPrimary: const Color(0xFFFFFFFF),
+                              primary: const Color(0xFF1877F2),
+                              minimumSize: const Size(120, 50),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            child: const Text(
+                              "Continuar pelo Facebook",
+                              style: TextStyle(
+                                fontSize: 17,
+                              ),
+                            ),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12, bottom: 20),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            onPrimary: const Color(0xFFFFFFFF),
+                            primary: const Color(0xFF000000),
+                            minimumSize: const Size(120, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                          child: const Text(
+                            "Continuar pelo Google",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-                padding: const EdgeInsets.only(top: 21),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: const Color(0xFFFFFFFF),
-                    primary: const Color(0xFF1877F2),
-                    minimumSize: const Size(120, 50),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  child: const Text(
-                    "Continuar pelo Facebook",
-                    style: TextStyle(
-                      fontSize: 17,
-                    ),
-                  ),
-                )),
-            Padding(
-              padding: const EdgeInsets.only(top: 12, bottom: 20),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  onPrimary: const Color(0xFFFFFFFF),
-                  primary: const Color(0xFF000000),
-                  minimumSize: const Size(120, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                child: const Text(
-                  "Continuar pelo Google",
-                  style: TextStyle(
-                    fontSize: 17,
-                  ),
-                ),
-              ),
-            ),
+            )
           ],
         ),
       ),
