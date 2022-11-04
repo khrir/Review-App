589c77a5e208bf3fd904240161ccf7a8ec8e54d3
Khrir <karllisson.15henrique@hotmail.com>
2022-11-04 13:50:16 -0300

    Refac bd.

--- lib/data/db_helper.dart
+++ lib/data/db_helper.dart
@@ -7,34 +7,35 @@ import 'package:sqflite/sqflite.dart';
 class DBHelper {
   initDB() async {
     String path = await getDatabasesPath();
-    String dbPath = join(path, "cards.db");
+    String dbPath = join(path, "review.db");
 
     Database db = await openDatabase(
       dbPath,
-      version: 2,
+      version: 1,
       onCreate: onCreate,
-      onUpgrade: onUpgrade,
     );
 
     return db;
   }
 
   Future<FutureOr<void>> onCreate(Database db, int version) async {
-    String sql = '''
-        CREATE TABLE materia (
-          id INTEGER PRIMARY KEY AUTOINCREMENT, 
-          name  VARCHAR(100));
-        ''';
+    String sql = 
+      '''
+      CREATE TABLE materia (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name  VARCHAR(100));
+      ''';
     await db.execute(sql);
 
-    sql = '''
-        CREATE TABLE questao (
-          id INTEGER AUTOINCREMENT PRIMARY KEY,
-          pergunta VARCHAR(100), 
-          resposta TEXT, 
-          id_materia INTEGER,
-          CONSTRAINT materia_fk FOREIGN KEY (id_materia) REFERENCES materia (id));
-        ''';
+    sql = 
+      '''
+      CREATE TABLE questao (
+        id INTEGER AUTOINCREMENT PRIMARY KEY,
+        pergunta VARCHAR(100), 
+        resposta TEXT, 
+        id_materia INTEGER,
+        CONSTRAINT materia_fk FOREIGN KEY (id_materia) REFERENCES materia (id));
+      ''';
     await db.execute(sql);
 
     sql = "INSERT INTO materia (name) VALUES ('Matemática');";
@@ -43,22 +44,18 @@ class DBHelper {
     sql =
         "INSERT INTO questao (pergunta, questao, id_materia) VALUES ('1 + 1', '2', 1);";
     await db.execute(sql);
-  }
 
-  Future<FutureOr<void>> onUpgrade(
-      Database db, int oldVersion, int newVersion) async {
-    if (oldVersion == 1 && newVersion == 2) {
-      String sql = '''
-        CREATE TABLE user (
-          id INTEGER PRIMARY KEY AUTOINCREMENT, 
-          email varchar(100), 
-          password varchar(100));
-        ''';
-      await db.execute(sql);
+    sql = 
+      '''
+      CREATE TABLE user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        email varchar(100), 
+        password varchar(100));
+      ''';
+    await db.execute(sql);
 
-      sql =
-          "INSERT INTO user (email, password) VALUES ('admin@gmail.com', 'admin')";
-      await db.execute(sql);
-    }
+    sql =
+        "INSERT INTO user (email, password) VALUES ('admin@gmail.com', 'admin')";
+    await db.execute(sql);
   }
 }
