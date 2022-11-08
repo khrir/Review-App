5e116378be2f09085b1811535986608cf4abc784
Khrir <karllisson.15henrique@hotmail.com>
2022-11-08 02:09:47 -0300

    Add fab icon.

--- lib/pages/home/home_widgets/floating_action_button/floating_action_button.dart
+++ lib/pages/home/home_widgets/floating_action_button/floating_action_button.dart
@@ -2,14 +2,15 @@ import "package:flutter/material.dart";
 import 'package:review_app/pages/cards/card_form.dart';
 
 FloatingActionButton getFloatingActionButton(context) {
-  return FloatingActionButton(onPressed: () {
-    Navigator.push(
-      context, 
-      MaterialPageRoute(
-        builder: (context) {
-          return const CardForm();
-        }
-      ),
-    );
-  });
+  return FloatingActionButton(
+      backgroundColor: const Color.fromARGB(131, 90, 2, 131),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const CardForm();
+          }),
+        );
+      },
+      child: const Icon(Icons.add));
 }
